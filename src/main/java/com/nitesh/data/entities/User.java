04ac0f489d2f0941909217f255095865cb7c4814
nitesh2054 <lognitesh97@gmail.com>
2019-03-30 14:38:44 +0545

    0303SimpleEntityMapping

--- src/main/java/com/nitesh/data/entities/User.java
+++ src/main/java/com/nitesh/data/entities/User.java
@@ -1,16 +1,38 @@
 package com.nitesh.data.entities;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name="FINANCES_USER")
 public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="USER_ID")
     private Long userId;
+
+    @Column(name="FIRST_NAME")
     private String firstName;
+
+    @Column(name="LAST_NAME")
     private String lastName;
+
+    @Column(name = "BIRTH_DATE")
     private Date birthDate;
+
+    @Column(name="EMAIL_ADDRESS")
     private String emailAddress;
+
+    @Column(name="LAST_UPDATED_DATE")
     private Date lastUpdatedDate;
+
+    @Column(name="LAST_UPDATED_BY")
     private String lastUpdatedBy;
+
+    @Column(name="CREATED_DATE")
     private Date createdDate;
+
+    @Column(name="CREATEDBY")
     private String createdBy;
 
     public Long getUserId() {
